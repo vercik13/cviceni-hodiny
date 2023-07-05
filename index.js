64bfae1c2cacccf47ad8e1a8a78463d37f97be99
Veronika Vokounová <ver.cik@seznam.cz>
2023-07-06 00:56:26 +0200

    aktualizace

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('funguju!');
+const times = [
+  {
+    hours: 12,
+    minutes: 34,
+  },
+
+  {
+    hours: 5,
+    minutes: 8,
+  },
+
+  {
+    hours: 7,
+    minutes: 8,
+  }
+
+]
+const Clock = (props) => {
+  const {hours, minutes} = props
+  const paddedHours = String(hours).padStart(2, '0');
+  const paddedMinutes = String(minutes).padStart(2, '0')
+  return `
+  <div class="clock">
+    <span class="clock__hours">${paddedHours}</span>:<span class="clock__minutes">${paddedMinutes}</span>
+  </div>
+  `
+}
+
+const renderClock = () => {
+  document.querySelector('#app').innerHTML = times.map(time => Clock(time)).join('')
+}
+
+renderClock()
